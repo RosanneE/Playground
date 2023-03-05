ddc8c2c1f8ed90e225de220aeb107b71fdf144b6
RosanneE <anderson.rosanne4@gmail.com>
2023-03-04 21:26:57 -0500

    tetris: css for squares, js one square show

--- src/pages/Tetris.js
+++ src/pages/Tetris.js
@@ -1,6 +1,20 @@
+import React from "react"
+import Grid from '../components/Tetris/Grid.js'
+
+import '../styles/tetris.css'
+
 function Tetris(props){
+
+
     return(
+        <div className="tetPage">
         <h1>Tetris</h1>
+
+        <div>
+            <Grid color = '1'/>
+        </div>
+      
+        </div>
     )
 }
 
