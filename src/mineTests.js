4504d4b7c8bba3f6213e089e361cfbff9b99a313
RosanneE <anderson.rosanne4@gmail.com>
2023-03-13 18:56:23 -0400

    TDD Setup for minesweeper

---
+++ src/mineTests.js
@@ -0,0 +1,11 @@
+// Test Driven Development for Minesweeper Game
+// import Mocha from 'mocha';
+const assert = require('assert')
+
+describe('+', () => {
+    it('returns the sum of its arguments', () => {
+  
+      assert.ok(3 + 4 === 7);
+      
+    });
+  });
